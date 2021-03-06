8b37d8fb67d738ef7d067ddfbf3f4eb283e4ff46
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-06 18:38:02 +0100

    Updates

---
+++ precache-manifest.f968a41a8e79f25bc23bf81255ab2344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9f3abef5fc0ebd95ee9561e9e66723",
+    "url": "/react-haushalts-buch/index.html"
+  },
+  {
+    "revision": "4f5bb5db15d7eb81e3b5",
+    "url": "/react-haushalts-buch/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7b9a67242060ca34a64c",
+    "url": "/react-haushalts-buch/static/css/main.7e570606.chunk.css"
+  },
+  {
+    "revision": "4f5bb5db15d7eb81e3b5",
+    "url": "/react-haushalts-buch/static/js/2.80b9f89b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-haushalts-buch/static/js/2.80b9f89b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b9a67242060ca34a64c",
+    "url": "/react-haushalts-buch/static/js/main.eb01fa6f.chunk.js"
+  },
+  {
+    "revision": "76362005d959eb8fa4ae",
+    "url": "/react-haushalts-buch/static/js/runtime-main.1080a9ba.js"
+  }
+]);
